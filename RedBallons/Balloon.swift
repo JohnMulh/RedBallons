6aaac8de638d9047bd61e7a04ea5f346f994fc0f
John Mulholland <jmulholland@John-iMac-2.local>
2014-11-16 19:28:55 +0000

    Completed Challenge

--- RedBallons/Balloon.swift
+++ RedBallons/Balloon.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import UIKit
+
+struct Balloon {
+    var image = UIImage(named: "")
+    var number = 0
+    
+    func assignImage () -> String
+    {
+        var randomNumber = Int(arc4random_uniform(UInt32(5)))
+        var randomImage: String
+        if randomNumber == 0 {
+            randomNumber = randomNumber + 1
+        }
+        switch randomNumber {
+        case 1:
+            randomImage = "RedBalloon1.jpg"
+        case 2:
+            randomImage = "RedBalloon2.jpg"
+        case 3:
+            randomImage = "RedBalloon3.jpg"
+        case 4:
+            randomImage = "RedBalloon4.jpg"
+        default:
+            randomImage = "BerlinTVTower.jpg"
+        }
+        return randomImage
+    }
+    
+}
--- RedBallons/ViewController.swift
+++ RedBallons/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// set up field to receive the balloons number
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var LabelName: UILabel!
+// setup array to hold all balloon instances
+    
+    var myBalloons:[Balloon] = []
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+// set up each balloon instance in myBalloons
+  
+        for currentIndex; currentIndex <= 99; currentIndex++
+        {
+/* create an instance of Balloon and insert number using currentIndex
+and get one of four random images by calling the assignImage function
+which will return one */
+        var aBalloon = Balloon()
+        var gotImage = aBalloon.assignImage()
+        aBalloon.image = UIImage(named: "\(gotImage)")
+        aBalloon.number = currentIndex
+// add the instance to the myBalloons array
+            myBalloons.append(aBalloon)}
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBallonButtonPressed(sender: UIBarButtonItem) {
+// generate a random number from the count of items in the myBalloons array check that it is not the same as the previously generated random number and if so get another random number
+        
+        var randomIndex: Int
+        
+        do {
+            randomIndex = Int(arc4random_uniform(UInt32(myBalloons.count)))
+        } while self.currentIndex == randomIndex
+        
+        self.currentIndex = randomIndex
+        
+        let balloon = myBalloons[self.currentIndex]
+        
+        self.LabelName.text = " The number of this item is \(balloon.number)"
+        self.myImageView.image = balloon.image
+    
+    }
 
 }
 
